5ab10266cef4bd81e351933648eb3bb4d5ecc16c
Arakawa Ryu <104044024+ryu-ua@users.noreply.github.com>
2024-06-14 13:51:53 +0900

    復習問題

--- fukuv0614/Form1.cs
+++ fukuv0614/Form1.cs
@@ -2,9 +2,42 @@ namespace fukuv0614
 {
     public partial class Form1 : Form
     {
+        static Random random = new Random();
+        
+        int count = 0;
+        int ans = random.Next(0, 51);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            int yosou;
+
+            yosou = int.Parse(textBox1.Text);
+            count++;
+
+            MessageBox.Show($"{count}âÒñ⁄ÇÃíßêÌ");
+
+            if (ans == yosou)
+            {
+                MessageBox.Show("Bingo!!");
+                count = 0;
+                ans = random.Next(0, 51);
+            }
+
+            else if (ans < yosou)
+            {
+                MessageBox.Show("Less");
+            }
+
+            else
+            {
+                MessageBox.Show("Greater");
+            }
         }
     }
 }
